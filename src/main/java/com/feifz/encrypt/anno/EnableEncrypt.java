5a3a8b2a10b3cecc42079237070256d854caef13
feifuzeng <feifz@tydic.com>
2018-09-29 14:43:09 +0800

    init commit

---
+++ src/main/java/com/feifz/encrypt/anno/EnableEncrypt.java
@@ -0,0 +1,34 @@
+package com.feifz.encrypt.anno;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.context.annotation.Import;
+import com.feifz.encrypt.auto.EncryptAutoConfiguration;
+
+/**
+ * 启用加密Starter
+ * 
+ * <p>在Spring Boot启动类上加上此注解<p>
+ * 
+ * <pre class="code">
+ * &#064;SpringBootApplication
+ * &#064;EnableEncrypt
+ * public class App {
+ *     public static void main(String[] args) {
+ *         SpringApplication.run(App.class, args);
+ *     }
+ * }
+ * <pre>
+ */
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Inherited
+@Import({EncryptAutoConfiguration.class})
+public @interface EnableEncrypt {
+
+}
---
+++ src/main/java/com/feifz/encrypt/auto/EncryptAutoConfiguration.java
@@ -0,0 +1,40 @@
+package com.feifz.encrypt.auto;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+import com.feifz.encrypt.advice.EncryptRequestBodyAdvice;
+import com.feifz.encrypt.advice.EncryptResponseBodyAdvice;
+
+/**
+ * 加解密自动配置
+ *
+ */
+@Configuration
+@Component
+@EnableAutoConfiguration
+@EnableConfigurationProperties(EncryptProperties.class)
+public class EncryptAutoConfiguration {
+
+	/**
+	 * 配置请求解密
+	 * @return
+	 */
+	@Bean
+	public EncryptResponseBodyAdvice encryptResponseBodyAdvice() {
+		return new EncryptResponseBodyAdvice();
+	}
+	
+	/**
+	 * 配置请求加密
+	 * @return
+	 */
+	@Bean
+	public EncryptRequestBodyAdvice encryptRequestBodyAdvice() {
+		return new EncryptRequestBodyAdvice();
+	}
+	
+}
